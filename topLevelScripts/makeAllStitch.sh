e64ca1b465a6c950a3272aff296a3f0c031306a5
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2014-03-13 23:57:48 -0500

    whitespace fix

--- topLevelScripts/makeAllStitch.sh
+++ topLevelScripts/makeAllStitch.sh
@@ -3,4 +3,4 @@ if [[ ! -d ${SUSHYFT_STITCHED_PATH} ]]; then
     mkdir -p ${SUSHYFT_STITCHED_PATH}
 fi
 rm -f ${SUSHYFT_STITCHED_PATH}/*.root
-ls $SUSHYFT_BASE/config/$SUSHYFT_MODE/stitchSystematicConfigs/*.cfg | parallel -n 1 -P 10  stitch.py ${SUSHYFT_REBIN_PATH} ${SUSHYFT_STITCHED_PATH} $SUSHYFT_BASE/config/$SUSHYFT_MODE/stitchConfig.cfg
+ls $SUSHYFT_BASE/config/$SUSHYFT_MODE/stitchSystematicConfigs/*.cfg | parallel -n 1 -P 10 stitch.py ${SUSHYFT_REBIN_PATH} ${SUSHYFT_STITCHED_PATH} $SUSHYFT_BASE/config/$SUSHYFT_MODE/stitchConfig.cfg
