5963c247af2809264ba2ec90772956caef011dd3
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2014-03-13 22:31:38 -0500

    fixing build targets

--- scripts/buildFitter.sh
+++ scripts/buildFitter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ${SUSHYFT_BASE}/src
-cd CLPTemplateMorph/ ; make install -j8
-cd ../CLPTemplateFitter/ ; make install -j8
-cd ../OptionUtils/ ; make install -j8
+cd CLPTemplateMorph/ ; make -j8
+cd ../CLPTemplateFitter/ ; make  -j8
+cd ../OptionUtils/ ; make -j8
 cd ../multiRegionFitter; make install -j8
