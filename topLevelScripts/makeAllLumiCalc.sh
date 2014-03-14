bd137ddb8fa0afd1374e46236721cd059d612254
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2014-03-14 02:09:18 -0500

    CRAB is pretty necessary for a lot of these scripts

--- topLevelScripts/makeAllLumiCalc.sh
+++ topLevelScripts/makeAllLumiCalc.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-# get the json
-toProcess=( )
-for DIR in ${SUSHYFT_EDNTUPLE_PATH}/*{MET,SingleMu}*; do
-    if [[ ! -d $DIR/res || ! -d $DIR/share ]]; then
-        continue
-    fi
-    toProcess+=("runIfChanged.sh $DIR/res/lumiSummary.json $DIR/res/*.xml -- crab -report -c $DIR")
-done
-echo "Executing ${#toProcess[@]} jobs"
-( for ((i = 0; i < ${#toProcess[@]}; i++)); do
-    echo "${toProcess[$i]}"
-done; ) | parallel -j 16 --eta --progress
+if [[ -z ${CRABDIR} ]]; then
+    echo "WARNING: No CRAB installation was sourced, this probably script needs"
+    echo "         CRAB to function."
+else
+    # get the json from crab
+    toProcess=( )
+    for DIR in ${SUSHYFT_EDNTUPLE_PATH}/*{MET,SingleMu}*; do
+        if [[ ! -d $DIR/res || ! -d $DIR/share ]]; then
+            continue
+        fi
+        toProcess+=("runIfChanged.sh $DIR/res/lumiSummary.json $DIR/res/*.xml -- crab -report -c $DIR")
+    done
+    echo "Executing ${#toProcess[@]} jobs"
+    ( for ((i = 0; i < ${#toProcess[@]}; i++)); do
+        echo "${toProcess[$i]}"
+    done; ) | parallel -j 16 --eta --progress
+fi
 
 # extract the lumi
 toProcess=( )
