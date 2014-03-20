8191021debb566cfc647959198e9a0b7bf0672fa
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2014-03-20 17:34:01 -0500

    sort the makeThesis script a bit more

--- topLevelScripts/makeThesis.sh
+++ topLevelScripts/makeThesis.sh
@@ -2,11 +2,27 @@
 
 # edmMakePhDThesis was a sham, this is the REAL thing.
 set -e
-makeAllFWLite.sh
-makeAllHadd.sh
-makeAllRebin.sh
-makeAllStitch.sh
-makeAllCopyHist.sh
-makeAllLumiCalc.sh
-makeAllQCDFits.sh
+if [[ -d ${SUSHYFT_EDNTUPLE_PATH} ]]; then
+    makeAllFWLite.sh
+fi
+if [[ -d ${SUSHYFT_FWLITE_PATH} ]]; then
+    makeAllHadd.sh
+fi
+if [[ -d ${SUSHYFT_HADD_PATH} ]]; then
+    makeAllRebin.sh
+fi
+if [[ -d ${SUSHYFT_REBIN_PATH} ]]; then
+    makeAllStitch.sh
+fi
+if [[ -d ${SUSHYFT_STITCHED_PATH} ]]; then
+    makeAllCopyHist.sh
+fi
+if [[ -d ${SUSHYFT_EDNTUPLE_PATH} ]]; then
+    makeAllLumiCalc.sh
+fi
+if [[ -d ${SUSHYFT_COPYHIST_PATH} ]]; then
+    makeAllQCDFits.sh
+fi
+
+# If you're not making the histograms, what's the point?
 makeAllFits.sh
