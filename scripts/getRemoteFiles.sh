8d96475f9e13d3cc9dbcaab9be094f0f40688f8f
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2014-06-11 17:11:18 -0500

    Try two for undoing then redoing the remove

--- scripts/getRemoteFiles.sh
+++ scripts/getRemoteFiles.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
-rsync -avh --progress --dry-run --exclude ".sushyft-*" ${SUSHYFT_REMOTE_PATH}/data/auto_hadd ${SUSHYFT_HADD_PATH}
+rsync -avh --progress --exclude ".sushyft-*" ${SUSHYFT_REMOTE_PATH}/data/auto_hadd ${SUSHYFT_HADD_PATH}
 rsync -avh ${SUSHYFT_REMOTE_PATH}/state/lumisum* ${SUSHYFT_STATE_PATH}
