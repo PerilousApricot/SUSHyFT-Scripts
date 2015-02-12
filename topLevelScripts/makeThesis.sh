7d6d38998c0addd2799361f86185ce79b4076a82
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2015-02-12 12:15:38 -0600

    remove systematic stuff

--- topLevelScripts/makeThesis.sh
+++ topLevelScripts/makeThesis.sh
@@ -14,17 +14,21 @@ fi
 if [[ -d ${SHYFT_REBIN_PATH} ]]; then
     makeAllStitch.sh
 fi
+plotStitched.py &
 if [[ -d ${SHYFT_STITCHED_PATH} ]]; then
     makeAllCopyHist.sh
 fi
 if [[ -d ${SHYFT_EDNTUPLE_PATH} ]]; then
-    makeAllLumiCalc.sh
+    #makeAllLumiCalc.sh
+    :
 fi
 if [[ -d ${SHYFT_COPYHIST_PATH} ]]; then
-    makeAllQCDFits.sh
+    #makeAllQCDFits.sh
+    :
 fi
 
-makeAllSystematics.sh
+#makeAllSystematics.sh
 
 # If you're not making the histograms, what's the point?
 makeAllFits.sh
+wait
