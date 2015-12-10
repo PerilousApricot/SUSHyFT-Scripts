1996bee9f864d1456ac330f23267aca0c490e34d
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2015-12-10 14:18:34 -0600

    Update remote files

--- scripts/getRemoteFiles.sh
+++ scripts/getRemoteFiles.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rsync -avh --progress --exclude ".shyft-*" --exclude ".sushyft-*" --delete -c ${SHYFT_REMOTE_PATH}/data/auto_hadd/*.root ${SHYFT_HADD_PATH}
-rsync -avh ${SHYFT_REMOTE_PATH}/state/lumisum* ${SHYFT_STATE_PATH}
+rsync -avh --progress --exclude ".shyft-*" --exclude ".sushyft-*" --delete -c "${SHYFT_REMOTE_PATH}/data/auto_rebin/*/*.root" ${SHYFT_REBIN_PATH}
+rsync -avh "${SHYFT_REMOTE_PATH}/state/lumisum*" ${SHYFT_STATE_PATH}
