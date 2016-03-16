ba19739224735ffcb688f9131c2829876d70968a
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2016-03-15 22:57:30 -0500

    5Jet limit fit

--- final_mar_15/make_shifted_systematics.sh
+++ final_mar_15/make_shifted_systematics.sh
@@ -7,22 +7,21 @@ else
     EXTRA_HIGGS=''
 fi
 mkdir -p higgs${EXTRA_HIGGS}/
+mkdir -p templates/{shifted${EXTRA_HIGGS},nofit${EXTRA_HIGGS},shift_noclean${EXTRA_HIGGS}}
 
 # Produce shifted systematic templates for higgs tool
-
-if is_stale higgs${EXTRA_HIGGS}/postfit_central.mrf central.mrf fitout/log_central.txt; then
-cp central.mrf higgs${EXTRA_HIGGS}/postfit_central.mrf
-awk '/^Fit Results:/{flag=1;next}/^$/{flag=0}flag {print "+ fixParamVal = " $2 "=" $4}' \
-    fitout/log_central.txt >> higgs${EXTRA_HIGGS}/postfit_central.mrf
-    if [[ "${HIGGS_NOSCALE:-0}" -ne 0 ]]; then
-        echo "+ fixParamVal = Stop450=1.0000" >> higgs${EXTRA_HIGGS}/postfit_central.mrf
-    fi
+if is_stale higgs${EXTRA_HIGGS}/postfit_central.mrf background_only.mrf fitout/log_background_only.txt; then
+    grep -v -e '_[1234]j_' -e '[1234]_Jet' background_only.mrf > higgs${EXTRA_HIGGS}/postfit_central.mrf.$$.tmp
+    awk '/^Fit Results:/{flag=1;next}/^$/{flag=0}flag {print "+ fixParamVal = " $2 "=" $4}' \
+        fitout/log_background_only.txt | grep -v 'qcd_[1234]j'  >> higgs${EXTRA_HIGGS}/postfit_central.mrf.$$.tmp
+    echo "+ names = Stop450" >> higgs${EXTRA_HIGGS}/postfit_central.mrf.$$.tmp
+    echo "+ fixParamVal = Stop450=1.0" >> higgs${EXTRA_HIGGS}/postfit_central.mrf.$$.tmp
+    replace_ifchanged higgs${EXTRA_HIGGS}/postfit_central.mrf higgs${EXTRA_HIGGS}/postfit_central.mrf.$$.tmp
 fi
-
-SHYFT_VALS=( "jes=0.05" "btag=0.10" "lftag=0.10" )
-
-#mv postfit_central.mrf higgs${EXTRA_HIGGS}/postfit_central.mrf
-mkdir -p templates/{shifted${EXTRA_HIGGS},nofit${EXTRA_HIGGS},shift_noclean${EXTRA_HIGGS}}
+grep -v '[1234]j' qcd_sf.mrf > qcd_sfTEST.mrf
+grep -v '[1234]j' lftag_sf.mrf > lftag_sfTEST.mrf
+grep -v '[1234]j' btag_sf.mrf > btag_sfTEST.mrf
+grep -v '[1234]j' jes_sf.mrf > jes_sfTEST.mrf
 
 # Rebin for higgs tool
 if is_stale higgs${EXTRA_HIGGS}/rebin_nominal.root rebin_higgs.config templates/nominal/nominal.root; then
@@ -30,6 +29,7 @@ if is_stale higgs${EXTRA_HIGGS}/rebin_nominal.root rebin_higgs.config templates/
                                 file=templates/nominal/nominal.root
 fi
 
+SHYFT_VALS=( "jes=0.05" "btag=0.10" "lftag=0.10" )
 for SHIFT in "${SHYFT_VALS[@]}"; do
     PARAM=${SHIFT%=*}
     VAL=${SHIFT#*=}
@@ -40,28 +40,22 @@ for SHIFT in "${SHYFT_VALS[@]}"; do
         sed "s/+ fixParamVal = ${PARAM}=.*/+ fixParamVal = ${PARAM}=${UP}/" \
             higgs${EXTRA_HIGGS}/postfit_central.mrf > \
             higgs${EXTRA_HIGGS}/postfit_${PARAM}Up.mrf
-            if [[ "${HIGGS_NOSCALE:-0}" -ne 0 ]]; then
-                echo "+ fixParamVal = Stop450=1.0000" >> higgs${EXTRA_HIGGS}/postfit_${PARAM}Up.mrf
-            fi
     fi
     if is_stale higgs${EXTRA_HIGGS}/postfit_${PARAM}Down.mrf higgs${EXTRA_HIGGS}/postfit_central.mrf; then
         sed "s/+ fixParamVal = ${PARAM}=.*/+ fixParamVal = ${PARAM}=${DOWN}/" \
             higgs${EXTRA_HIGGS}/postfit_central.mrf > \
             higgs${EXTRA_HIGGS}/postfit_${PARAM}Down.mrf
-            if [[ "${HIGGS_NOSCALE:-0}" -ne 0 ]]; then
-                echo "+ fixParamVal = Stop450=1.0000" >> higgs${EXTRA_HIGGS}/postfit_${PARAM}Down.mrf
-            fi
     fi
     for DIR in Up Down; do
         runIfChanged.sh templates/shift_noclean${EXTRA_HIGGS}/${PARAM}${DIR}_lum1.0_templates.root \
                 higgs${EXTRA_HIGGS}/postfit_${PARAM}${DIR}.mrf \
                 higgs${EXTRA_HIGGS}/rebin_nominal.root \
-                qcd_sf.mrf btag_sf.mrf lftag_sf.mrf jes_sf.mrf \
+                qcd_sfTEST.mrf btag_sfTEST.mrf lftag_sfTEST.mrf jes_sfTEST.mrf \
             -- \
             multiRegionFitter.exe higgs${EXTRA_HIGGS}/postfit_${PARAM}${DIR}.mrf \
                 templateFile=higgs${EXTRA_HIGGS}/rebin_nominal.root dominos=1 \
                 output=templates/shift_noclean${EXTRA_HIGGS}/${PARAM}${DIR} savePlots=1 saveTemplates=1 \
-                includefiles=qcd_sf.mrf,btag_sf.mrf,lftag_sf.mrf,jes_sf.mrf \
+                includefiles=qcd_sfTEST.mrf,btag_sfTEST.mrf,lftag_sfTEST.mrf,jes_sfTEST.mrf \
                 showcorrelations=1 fitData=1
         runIfChanged.sh higgs${EXTRA_HIGGS}/input_${PARAM}${DIR}.root \
                         fitout_cleanup.config \
@@ -72,26 +66,8 @@ for SHIFT in "${SHYFT_VALS[@]}"; do
                             higgs${EXTRA_HIGGS}/input_${PARAM}${DIR}.root
     done
 done
-# Bring over the nomnial histogram too
-runIfChanged.sh templates/shift_noclean${EXTRA_HIGGS}/nominal_lum1.0_templates.root \
-            higgs${EXTRA_HIGGS}/postfit_central.mrf \
-            higgs${EXTRA_HIGGS}/rebin_nominal.root \
-            qcd_sf.mrf btag_sf.mrf lftag_sf.mrf jes_sf.mrf \
-        -- \
-        multiRegionFitter.exe higgs${EXTRA_HIGGS}/postfit_central.mrf \
-            templateFile=higgs${EXTRA_HIGGS}/rebin_nominal.root dominos=1 \
-            output=templates/shift_noclean${EXTRA_HIGGS}/nominal savePlots=1 saveTemplates=1 \
-            includefiles=qcd_sf.mrf,btag_sf.mrf,lftag_sf.mrf,jes_sf.mrf \
-            showcorrelations=1 fitData=1
-runIfChanged.sh higgs${EXTRA_HIGGS}/input_nominal.root \
-                    fitout_cleanup.config \
-                    templates/shift_noclean${EXTRA_HIGGS}/nominal_lum1.0_templates.root \
-        -- \
-        copyHistograms.py fitout_cleanup.config \
-                        file=templates/shift_noclean${EXTRA_HIGGS}/nominal_lum1.0_templates.root \
-                        higgs${EXTRA_HIGGS}/input_nominal.root
 
-NOSHYFT_VALS=( "jec090=jecDown" "jec110=jecUp" )
+NOSHYFT_VALS=( "nominal=nominal" "jec090=jecDown" "jec110=jecUp" )
 if [[ "$(basename $(dirname $(pwd)))" == "OLD" ]]; then
     :
 else
@@ -102,30 +78,31 @@ else
         done
     done
 fi
-mkdir -p templates/{noshift_noclean,}
+echo "Processing " "${NOSHYFT_VALS[@]}"
+mkdir -p templates/{shift_noclean,}
 for SHIFT in "${NOSHYFT_VALS[@]}"; do
     PARAM=${SHIFT%=*}
     VAL=${SHIFT#*=}
-    if is_stale higgs${EXTRA_HIGGS}/rebin_${PARAM}.root rebin_higgs.config templates/nominal/${PARAM}.root; then
+    if is_stale higgs${EXTRA_HIGGS}/rebin_${PARAM}.root rebin_higgs.config templates/nominal/${PARAM}.root ; then
         copyHistograms.py rebin_higgs.config higgs${EXTRA_HIGGS}/rebin_${PARAM}.root \
                                 file=templates/nominal/${PARAM}.root
     fi
-    runIfChanged.sh templates/noshift_noclean${EXTRA_HIGGS}/${PARAM}_lum1.0_templates.root \
+    runIfChanged.sh templates/shift_noclean${EXTRA_HIGGS}/${PARAM}_lum1.0_templates.root \
                 higgs${EXTRA_HIGGS}/postfit_central.mrf \
                 higgs${EXTRA_HIGGS}/rebin_${PARAM}.root \
-                qcd_sf.mrf btag_sf.mrf lftag_sf.mrf jes_sf.mrf \
+                qcd_sfTEST.mrf btag_sfTEST.mrf lftag_sfTEST.mrf jes_sfTEST.mrf \
             -- \
             multiRegionFitter.exe higgs${EXTRA_HIGGS}/postfit_central.mrf \
                 templateFile=higgs${EXTRA_HIGGS}/rebin_${PARAM}.root dominos=1 \
-                output=templates/noshift_noclean${EXTRA_HIGGS}/${PARAM} savePlots=1 saveTemplates=1 \
-                includefiles=qcd_sf.mrf,btag_sf.mrf,lftag_sf.mrf,jes_sf.mrf \
+                output=templates/shift_noclean${EXTRA_HIGGS}/${PARAM} savePlots=1 saveTemplates=1 \
+                includefiles=qcd_sfTEST.mrf,btag_sfTEST.mrf,lftag_sfTEST.mrf,jes_sfTEST.mrf \
                 showcorrelations=1 fitData=1
     runIfChanged.sh higgs${EXTRA_HIGGS}/input_${VAL}.root \
                     fitout_cleanup.cfg \
-                    templates/noshift_noclean${EXTRA_HIGGS}/${PARAM}_lum1.0_templates.root \
+                    templates/shift_noclean${EXTRA_HIGGS}/${PARAM}_lum1.0_templates.root \
             -- \
             copyHistograms.py fitout_cleanup.config \
-                            file=templates/noshift_noclean${EXTRA_HIGGS}/${PARAM}_lum1.0_templates.root \
+                            file=templates/shift_noclean${EXTRA_HIGGS}/${PARAM}_lum1.0_templates.root \
                             higgs${EXTRA_HIGGS}/input_${VAL}.root
 done
 
